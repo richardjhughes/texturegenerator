56cd6d5e13e034301f914e2bbc5c084a6a95934c
Richard Hughes <richard.j.hughes@hotmail.com>
2021-08-21 21:49:25 -0400

    Add `version.h`.

--- src/runner/main.cpp
+++ src/runner/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
+#include "version.h"
 #include "nodes/create/create.h"
 
 int main(int, char*[]) {
-    std::cout << "Runner\n";
+    std::cout << "Runner\n" << PROJECT_NAME << " " << PROJECT_VERSION;
 
     test();
 
